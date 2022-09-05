f523fe56b675c066859dea27cfc142dc5e48d34b
Dio <dioginisaugusto1@gmail.com>
2022-09-05 18:04:12 -0300

    somen components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <SalesCard />
+          </div>
+        </section>
+      </main>
+
+    </div>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import vector from '../../assets/img/Vector.svg';
+import './styles.css'
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={vector} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
